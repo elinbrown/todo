df9946fe9e420ab88a2cd2bd44ed8f1a0e2a6fa4
elinbrown <elin@elinbrown.nu>
2019-03-03 09:42:10 +0100

    added header title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div id="app">
+    <!-- container -->
     <div class="container">
+      <!-- nav -->
+      <nav class="level">
+        <p class="level-item has-text-centered">
+          <span class="app-title">TODO</span>
+        </p>
+      </nav>
+      <!-- end nav -->
+
+      <!-- components --> 
       <add-todo></add-todo>
       <todo-list></todo-list>
+      <!-- end components --> 
+
+      <!-- footer -->
+      <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Todo</strong> by Elin Brown.<br>
+          <a href="https://github.com/elinbrown/todo" target="_blank">Github repo</a>
+        </p>
+      </div>
+    </footer>
+    <!-- end footer -->
     </div>
+    <!-- end container -->
   </div>
 </template>
 
@@ -22,6 +45,7 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400');
+@import url('https://fonts.googleapis.com/css?family=Oswald');
 
 body, html {
   font-family: 'Open Sans', sans-serif; 
@@ -31,6 +55,26 @@ body, html {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding: 40px;
+}
+
+.container
+{
+  margin-top: 20px;
+  max-width: 50%;
+}
+
+.app-title
+{
+  font-family: 'Oswald';
+  font-size: 2em; 
+}
+
+.footer {
+  background-color: #FFF;
+}
+
+.navbar
+{
+  background-color: #f1f1f1;
 }
 </style>
